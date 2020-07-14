cf5009fc4bab1b98ee888b2926deab3934782b99
MatheusEli <matheuselisouzaferreira@hotmail.com>
2020-07-14 19:34:30 -0300

    Adicionando classes DAO, Adapter e Nota

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -3,8 +3,15 @@ package br.com.alura.ceep.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.List;
 
 import br.com.alura.ceep.R;
+import br.com.alura.ceep.dao.NotaDAO;
+import br.com.alura.ceep.model.Nota;
+import br.com.alura.ceep.ui.adapter.ListaNotasAdapter;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -12,5 +19,13 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+
+        ListView listview = findViewById(R.id.listView);
+        NotaDAO dao = new NotaDAO();
+
+        dao.insere(new Nota("Alemão","Ich bin Matheus: Eu sou Matheus, I habe hunger: Eu tenho fome, Ich mag Deutschland: Eu gosto da Alemanha."));
+
+        List<Nota> listaNotas = dao.todos();
+        listview.setAdapter(new ListaNotasAdapter(this,listaNotas));
     }
 }
