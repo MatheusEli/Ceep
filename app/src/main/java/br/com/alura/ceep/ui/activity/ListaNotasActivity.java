d1d5f105bd0ef721931a5a73831171a17e413475
MatheusEli <matheuselidesouzaferreira@hotmail.com>
2020-07-14 11:17:43 -0300

    Adicionando txtview em activity_lista_notas.xml

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import br.com.alura.ceep.R;
+//LISTA NOTAS ACTIVITY
 
 public class ListaNotasActivity extends AppCompatActivity {
 
